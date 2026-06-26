60f01860c2ac03bee5824e92fb0a13de54ffd3d4
Chris <cristianocorona2424@gmail.com>
2026-06-26 11:55:04 +0200

    Added the optional loss parameter "alpha" to the Beamsplitter class.
    This is set to zero by default to ensure compatibility with the previous work.
    It is useful to perform fidelity related studies with respect to the loss tolerance.
    
    Co-authored-by: Daniele Confalonieri <daniele2.confalonieri@mail.polimi.it>

--- interferometer/main.py
+++ interferometer/main.py
@@ -6,29 +6,40 @@ class Beamsplitter:
 
     The matrix describing the mode transformation is:
 
-        e^{iphi}*cos(theta)     -sin(theta)
-        e^{iphi}*sin(theta)     cos(theta)
+    e^{-alpha} * [[e^{iphi}*cos(theta)     -sin(theta)],
+                  [e^{iphi}*sin(theta)     cos(theta)]]
 
     Args:
         mode1 (int): the index of the first mode (the first mode is mode 1)
         mode2 (int): the index of the second mode
         theta (float): the beam splitter angle
         phi (float): the beam splitter phase
+        alpha (float): the beam splitter loss parameter (default 0)
     """
 
-    def __init__(self, mode1, mode2, theta, phi):
+    def __init__(self, mode1, mode2, theta, phi, alpha=0.0):
         self.mode1 = mode1
         self.mode2 = mode2
         self.theta = theta
         self.phi = phi
+        self.alpha = alpha
 
     def __repr__(self):
-        repr = "\n Beam splitter between modes {} and {}: \n Theta angle: {:.2f} \n Phase: {:.2f}".format(
-            self.mode1,
-            self.mode2,
-            self.theta,
-            self.phi
-            )
+        if self.alpha == 0.0:
+            repr = "\n Beam splitter between modes {} and {}: \n Theta angle: {:.2f} \n Phase: {:.2f}".format(
+                self.mode1,
+                self.mode2,
+                self.theta,
+                self.phi
+                )
+        else:
+            repr = "\n Beam splitter between modes {} and {}: \n Theta angle: {:.2f} \n Phase: {:.2f} \n Loss parameter: {:.2f}".format(
+                self.mode1,
+                self.mode2,
+                self.theta,
+                self.phi,
+                self.alpha
+                )
         return repr
 
 
